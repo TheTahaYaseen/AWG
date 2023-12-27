f5778c6e920430dc6c859e30d3651d73dea166cb
The Taha Yaseen <thetahayaseen@gmail.com>
2023-12-28 02:49:17 +0500

    Standardization

---
+++ pseudocode.py
@@ -0,0 +1,28 @@
+# Pseudocode
+
+"""
+
+Home Page (Will Be Made Last With Graphical And Textual Instructions On How To Play The Game)
+Standard Auth (Username And Password [Add Proper Validation Later])
+
+Valid Game Logic: User Given Spaces According To Word Length
+
+Words Database (Just The Word With Its Definition)
+Difficulty Will Increase By Decreasing Time
+Just Some Allgorithm For Deciding Score Based On Difficulty And Word Length
+Acheivements To Be Acheived When User Reaches Certain Points Or Streaks
+Leaderboard Based On Users Having Maximum Score Points
+Hints To Be Given By Definition When User Has Only 3 Attempts Left
+Educational Portion For Users To See Words With Definitions, Usage Examples Or To Do Some Trivia [An Acheivement Can Also Be Unlocked For Trivia]
+
+Admin Panel:
+    Feedbacks Shown
+    For Deleting Or Making User Admin
+    Words With Definition And Usage Examples
+    Trivia Questions With Answers (Acheivement Associated Can Also Be Made)
+    Acheivements (By Points Or Streak)
+
+Contact Us & Feedback For User: So That He Can Give Feedback To Us And That To Be Displayed In Admin Panel
+
+"""
+
